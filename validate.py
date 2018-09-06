8bfc0bdfd7f9bded5cf813521e0a22965c31c09b
Anne Königs <anne.koenigs@hotmail.de>
2018-09-06 15:18:05 +0200

    first attempt at validating against automatically generated DTD, not yet successfull

---
+++ validate.py
@@ -0,0 +1,15 @@
+from lxml import etree
+
+# xml_decl = '<?xml version="1.0" encoding="UTF-8"?>'
+# mk_doctype = '<!DOCTYPE SYSTEM tei_ha_digipub_ak.dtd>'
+
+my_file = 'edtion_bretke_asarie.tei'
+
+parser = etree.XMLParser(dtd_validation=True)
+root = etree.parse(my_file, parser).getroot()
+
+# outstr = xml_decl + '\n' + mk_doctype + '\n' + etree.tostring(root, encoding="unicode", pretty_print=True)
+# with open(outfile, 'w') as sd:
+#     sd.write(outstr)
+
+# TODO: Korrekte Verarbeitung von Unicode überprüfen! - sieht erstmal ganz gut aus...
